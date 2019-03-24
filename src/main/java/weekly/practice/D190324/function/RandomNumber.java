94d14f31204b7b9d8c9e82e9746b6cc7abc15dd0
wenjun.zhou <wenjun.zhou@lazada.com>
2019-03-24 20:22:52 +0800

    Add D190324

--- src/main/java/weekly/practice/D190324/function/RandomNumber.java
+++ src/main/java/weekly/practice/D190324/function/RandomNumber.java
@@ -1,51 +1,34 @@
 package weekly.practice.D190324.function;
 
-import java.util.LinkedList;
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 /**
  * @ClassName: RandomNumber
  * @Description:
  * @author: hengsheng
- * @create: 2019-03-17 14:34
+ * @create: 2019-03-24 14:34
  **/
 public class RandomNumber {
 
     public static void main(String[] args) {
-
-
-
-
         Random random = new Random();
-        /*
-        1、step为1，即生成1~100的随机数
-        2、step为2，即生成1~200的基数
-        3、以此类推
-         */
-        int start = 1, step = 1, end = 100;
-        List<Integer> originalList = Stream.iterate(start, i -> i + step).limit(end).collect(Collectors.toCollection(LinkedList::new));
-
-//add comment by Ethan
-        //这块的代码写得乱，看一下JDK，用IntStream.
-
-
-
-        System.out.println("originalList: " + originalList);
-        List<Integer> result = Stream.iterate(start, i -> i + step).limit(end)
-                .map(i -> originalList.remove(random.nextInt(((end + 1) * step - i) / step))).collect(Collectors.toList());
-
-        //这里写的也不工整。
-
-
-        System.out.println("size: " + result.size());
+        List<Integer> list = Lists.newLinkedList();
+        List<Integer> result = Lists.newArrayList();
+        int start = 1, end = 101;
+        IntStream.range(start, end).forEach(list::add);
+        IntStream.range(start, end).forEach(i -> result.add(list.remove(random.nextInt(end - i))));
         System.out.println("result: " + result);
-        System.out.println("sortedResult: " + result.stream().sorted().collect(Collectors.toList()));
+        Assert.assertEquals(100, result.size());
+        Assert.assertEquals(0, list.size());
     }
 
-// add comment by Ethan
+    // add comment by Ethan
     //1.代码行数少
     //2.代码质量还有待提升，写的不工整，Sout写的太多，太乱
     //3.让你做这些例子，不仅是提升你的编程技巧，也要提升代码质量和风格
